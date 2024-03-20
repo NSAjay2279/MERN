164a9614edcac8c87f8f4d6bed63857ceecb7f2f
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-20 15:51:27 +0000

    Wed, Mar 20, 2024, 9:21 PM +05:30

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+function drawBall(x, y, r){
+    ctx.beginPath();
+    ctx.arc(100, 100, 20, 0, 2 * Math.PI);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.fillStyle = "red";
+    ctx.fill();
+}
 
-ctx.beginPath();
-ctx.arc(100, 100, 20, 0, 2*Math.PI);
-ctx.strokeStyle = "black";
-ctx.stroke();
-ctx.fillStyle = "red";
-ctx.fill();
+drawBall(100, 100, 20);
+drawBall(200, 200, 30);
